5f3ec189b5621991e06ab97891764693c8108df5
Georgi Terziev <terzata2002@abv.bg>
2019-09-25 19:56:31 +0300

    1-14 done

--- Built-In Functions/Exercise/Built In Functions exercises.sql
+++ Built-In Functions/Exercise/Built In Functions exercises.sql
@@ -57,3 +57,35 @@ SELECT FirstName, LastName
     FROM Employees
    WHERE Salary BETWEEN 10000 AND 50000
 ORDER BY Salary DESC
+
+-- Exercise 11
+SELECT * FROM
+   (SELECT EmployeeID, FirstName, LastName, Salary, DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+     FROM Employees
+    WHERE Salary BETWEEN 10000 AND 50000) AS temp
+   WHERE temp.[Rank] = 2
+ORDER BY temp.Salary DESC
+
+USE [Geography]
+-- Exercise 12
+  SELECT CountryName, IsoCode FROM Countries
+   WHERE CountryName LIKE '%a%a%a%'
+ORDER BY IsoCode
+
+-- Exercise 13
+  SELECT p.PeakName, r.RiverName, LOWER(CONCAT(LEFT(p.PeakName, LEN(p.PeakName)-1), r.RiverName)) AS [Mix] 
+    FROM Peaks AS p, Rivers AS r
+   WHERE RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
+ORDER BY [Mix]
+
+  SELECT p.PeakName, r.RiverName, LOWER(CONCAT(LEFT(p.PeakName, LEN(p.PeakName)-1), r.RiverName)) AS [Mix]  
+    FROM Peaks AS p
+JOIN Rivers AS r ON RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
+ORDER BY [Mix]
+
+USE Diablo
+--Exercise 14
+  SELECT TOP(50) [Name], FORMAT([Start], 'yyyy-MM-dd') AS [Start]
+    FROM Games
+   WHERE DATEPART([YEAR], [Start]) IN (2011, 2012)
+ORDER BY [Start]
