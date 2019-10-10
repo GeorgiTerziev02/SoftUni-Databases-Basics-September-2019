4ffe8dbcdea327cfdf5f1fe5bf59cf55ce2a4bdf
Georgi Terziev <terzata2002@abv.bg>
2019-10-10 19:58:57 +0300

    HW done

--- Database Programmability/Exercises/Exercises.sql
+++ Database Programmability/Exercises/Exercises.sql
@@ -229,10 +229,25 @@ END
 EXEC usp_CalculateFutureValueForAccount 1, 0.1
 
 USE [Diablo]
+
+GO
 -- Exercise 13
-CREATE FUNCTION ufn_CashInUsersGames (@game NVARCHAR(100))
-RETURNS TABLE
+CREATE FUNCTION ufn_CashInUsersGames (@game VARCHAR(MAX))
+RETURNS @output TABLE (SumCash DECIMAL(18, 4))
 AS
 BEGIN
-	
+	INSERT INTO @output SELECT(
+	SELECT SUM(Cash) AS [SumCash] FROM(
+		SELECT *, ROW_NUMBER() OVER (ORDER BY us.Cash DESC) AS [RowNum]
+		FROM UsersGames us
+		WHERE GameId IN (
+		SELECT Id 
+			FROM Games
+		WHERE [Name] = @game
+		)) AS [RowNumTable]
+	WHERE [RowNumTable].RowNum % 2 <> 0)
+
+	RETURN
 END
+
+SELECT * FROM dbo.ufn_CashInUsersGames ('Love in a mist')
