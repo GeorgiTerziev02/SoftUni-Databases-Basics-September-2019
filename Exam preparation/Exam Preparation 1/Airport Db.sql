aa245d9576a7b2616ff05fff921958c886c2183d
Georgi Terziev <terzata2002@abv.bg>
2019-10-12 18:20:08 +0300

    exam prep

---
+++ Exam preparation/Exam Preparation 1/Airport Db.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE Airport
+
+USE Airport
+
+CREATE TABLE Planes(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL,
+	Seats INT NOT NULL,
+	[Range] INT NOT NULL
+)
+
+CREATE TABLE Flights(
+	Id INT PRIMARY KEY IDENTITY,
+	DepartureTime DATETIME,
+	ArrivalTime DATETIME,
+	Origin VARCHAR(50) NOT NULL,
+	Destination VARCHAR(50) NOT NULL,
+	PlaneId INT FOREIGN KEY REFERENCES Planes(Id) NOT NULL
+)
+
+CREATE TABLE Passengers(
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(30) NOT NULL,
+	LastName VARCHAR(30) NOT NULL,
+	Age INT NOT NULL,
+	[Address] VARCHAR(30) NOT NULL,
+	PassportId CHAR(11) NOT NULL
+)
+
+CREATE TABLE LuggageTypes(
+	Id INT PRIMARY KEY IDENTITY,
+	[Type] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Luggages(
+	Id INT PRIMARY KEY IDENTITY,
+	LuggageTypeId INT FOREIGN KEY REFERENCES LuggageTypes(Id) NOT NULL,
+	PassengerId INT FOREIGN KEY REFERENCES Passengers(Id) NOT NULL
+)
+
+CREATE TABLE Tickets(
+	Id INT PRIMARY KEY IDENTITY,
+	PassengerId INT FOREIGN KEY REFERENCES Passengers(Id) NOT NULL,
+	FlightId INT FOREIGN KEY REFERENCES Flights(Id) NOT NULL,
+	LuggageId INT FOREIGN KEY REFERENCES Luggages(Id) NOT NULL,
+	Price DECIMAL(18, 2) NOT NULL
+)
+
+-- Exercise 2
+INSERT INTO Planes ([Name], Seats, [Range]) VALUES
+	('Airbus 336', 112, 5132),
+	('Airbus 330', 432, 5325),
+	('Boeing 369', 231, 2355),
+	('Stelt 297', 254, 2143),
+	('Boeing 338', 165, 5111),
+	('Airbus 558', 387, 1342),
+	('Boeing 128', 345, 5541)
+
+INSERT INTO LuggageTypes ([Type]) VALUES
+	('Crossbody Bag'),
+	('School Backpack'),
+	('Shoulder Bag')
+	
+SELECT * FROM LuggageTypes 
+
+-- Exercise 3
+UPDATE Tickets
+SET Price += t.Price * 0.13
+FROM Tickets t
+JOIN Flights f ON f.Id = t.FlightId
+WHERE f.Destination = 'Carlsbad'
+
+-- Exercise 4
+DELETE FROM Tickets
+ WHERE FlightId IN (SELECT Id FROM Flights
+			WHERE Destination = 'Ayn Halagim'
+ )
+
+DELETE FROM Flights 
+ WHERE Destination = 'Ayn Halagim' 
+
