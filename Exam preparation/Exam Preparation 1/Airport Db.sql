4ef260aad8ea16a521d732b96fe484ae34e93526
Georgi Terziev <terzata2002@abv.bg>
2019-10-13 16:19:54 +0300

    Update Airport Db.sql

--- Exam preparation/Exam Preparation 1/Airport Db.sql
+++ Exam preparation/Exam Preparation 1/Airport Db.sql
@@ -79,3 +79,89 @@ DELETE FROM Tickets
 DELETE FROM Flights 
  WHERE Destination = 'Ayn Halagim' 
 
+-- Exercise 5 
+SELECT *
+  FROM Planes
+ WHERE [Name] LIKE '%tr%'
+ORDER BY Id, [Name], Seats, [Range]
+
+-- Exercise 6
+  SELECT FlightId, SUM(Price) AS [Price]
+    FROM Tickets
+GROUP BY FlightId
+ORDER BY SUM(Price) DESC, FlightId 
+
+-- Exercise 7
+  SELECT CONCAT(p.FirstName, ' ', p.LastName) AS [Full Name] , f.Origin, f.Destination
+    FROM Passengers p 
+    JOIN Tickets t ON p.Id = t.PassengerId
+    JOIN Flights f ON f.Id = t.FlightId
+ORDER BY [Full Name], f.Origin, f.Destination
+
+-- Exercise 8
+   SELECT FirstName, LastName, Age
+     FROM Passengers p
+LEFT JOIN Tickets t ON p.Id = t.PassengerId
+    WHERE t.Id IS NULL
+ ORDER BY Age DESC, FirstName, LastName
+
+-- Exercise 9
+  SELECT CONCAT(p.FirstName, ' ', p.LastName) AS [Full Name], pl.[Name], CONCAT(f.Origin, ' - ', f.Destination) AS [Trip], lt.[Type] AS [Luggage Type]
+    FROM Passengers p
+	JOIN Tickets t ON t.PassengerId = p.Id
+    JOIN Flights f ON t.FlightId = f.Id
+    JOIN Planes pl ON f.PlaneId = pl.Id
+    JOIN Luggages l ON l.Id = t.LuggageId
+    JOIN LuggageTypes lt ON lt.Id = l.LuggageTypeId
+ORDER BY [Full Name], pl.[Name], f.Origin, f.Destination, [Luggage Type]
+
+-- Exercise 10
+SELECT p.[Name], p.Seats, COUNT(t.Id) AS [PassengersCount]
+  FROM Planes p
+LEFT JOIN Flights AS f ON f.PlaneId = p.Id
+LEFT JOIN Tickets AS t ON t.FlightId = f.Id
+GROUP BY p.[Name], p.Seats
+ORDER BY PassengersCount DESC, p.Name, p.Seats
+
+-- Exercise 11
+CREATE FUNCTION udf_CalculateTickets(@origin VARCHAR(MAX), @destination VARCHAR(MAX), @peopleCount INT) 
+RETURNS VARCHAR(100)
+AS
+BEGIN
+	IF(@peopleCount <= 0)
+	BEGIN
+		RETURN 'Invalid people count!'
+	END
+
+	DECLARE @ticketPrice DECIMAL(18,2) = (
+		SELECT t.Price 
+		  FROM Tickets t
+		  JOIN Flights f ON t.FlightId = f.Id
+		 WHERE f.Origin = @origin AND f.Destination = @destination
+	)
+
+	IF(@ticketPrice IS NULL)
+	BEGIN
+		RETURN 'Invalid flight!'
+	END
+
+	DECLARE @total DECIMAL(18, 2) = @ticketPrice * @peopleCount
+
+	RETURN CONCAT('Total price ', @total)
+END
+
+SELECT dbo.udf_CalculateTickets('Kolyshley','Rancabolang', 33)
+SELECT dbo.udf_CalculateTickets('Kolyshley','Rancabolang', -1)
+SELECT dbo.udf_CalculateTickets('Invalid','Rancabolang', 33)
+
+
+-- Exercise 12
+CREATE PROCEDURE usp_CancelFlights
+AS
+BEGIN
+	UPDATE Flights
+	SET DepartureTime = NULL, ArrivalTime = NULL
+	WHERE DATEDIFF(SECOND, DepartureTime, ArrivalTime) > 0
+END
+
+EXEC usp_CancelFlights
