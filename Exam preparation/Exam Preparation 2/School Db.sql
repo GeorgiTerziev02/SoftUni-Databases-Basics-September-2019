232bab2ca4e14225527a2b2010ba5509a16c1b38
Georgi Terziev <terzata2002@abv.bg>
2019-10-16 21:06:57 +0300

    exam prep 2

--- Exam preparation/Exam Preparation 2/School Db.sql
+++ Exam preparation/Exam Preparation 2/School Db.sql
@@ -167,5 +167,22 @@ BEGIN
 		RAISERROR('This school has no student with the provided id!', 16, 1)
 		RETURN
 	END
+	 
+	DELETE FROM StudentsExams
+	WHERE StudentId = @StudentId
+
+	DELETE FROM StudentsSubjects
+	WHERE StudentId = @StudentId
+
+	DELETE FROM StudentsTeachers
+	WHERE StudentId = @StudentId
+
+	DELETE FROM Students
+	WHERE Id = @StudentId
 
 END
+
+EXEC usp_ExcludeFromSchool 301
+
+EXEC usp_ExcludeFromSchool 1
+SELECT COUNT(*) FROM Students
