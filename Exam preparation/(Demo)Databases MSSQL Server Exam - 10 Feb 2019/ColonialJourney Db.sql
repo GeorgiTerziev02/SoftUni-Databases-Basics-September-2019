e2bbedd78f5d0007b6863cd21cf0913a6c0b7519
Georgi Terziev <terzata2002@abv.bg>
2019-10-16 23:17:45 +0300

    (Demo)Databases MSSQL Server Exam - 10 Feb 2019

---
+++ Exam preparation/(Demo)Databases MSSQL Server Exam - 10 Feb 2019/ColonialJourney Db.sql
@@ -0,0 +1,129 @@
+CREATE DATABASE ColonialJourney 
+
+USE ColonialJourney 
+
+CREATE TABLE Planets(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Spaceports(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	PlanetId INT FOREIGN KEY REFERENCES Planets(Id) NOT NULL
+)
+
+CREATE TABLE Spaceships(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	Manufacturer VARCHAR(30) NOT NULL,
+	LightSpeedRate INT DEFAULT 0
+)
+
+CREATE TABLE Colonists(
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(20) NOT NULL,
+	LastName VARCHAR(20) NOT NULL,
+	Ucn VARCHAR(10) UNIQUE NOT NULL,
+	BirthDate DATE NOT NULL
+)
+
+CREATE TABLE Journeys(
+	Id INT PRIMARY KEY IDENTITY,
+	JourneyStart DATETIME2 NOT NULL,
+	JourneyEnd DATETIME2 NOT NULL,
+	Purpose VARCHAR(11),
+	CONSTRAINT CHK_Purpose CHECK (Purpose IN ('Medical', 'Technical', 'Educational', 'Military')),
+	DestinationSpaceportId INT FOREIGN KEY REFERENCES Spaceports(Id) NOT NULL,
+	SpaceshipId INT FOREIGN KEY REFERENCES Spaceships(Id) NOT NULL
+)
+
+CREATE TABLE TravelCards(
+	Id INT PRIMARY KEY IDENTITY,
+	CardNumber CHAR(10) UNIQUE NOT NULL,
+	JobDuringJourney VARCHAR(8) NOT NULL,
+	CONSTRAINT CHK_JobDuringJourney CHECK (JobDuringJourney IN ('Pilot', 'Engineer', 'Trooper', 'Cleaner', 'Cook')),
+	ColonistId INT FOREIGN KEY REFERENCES Colonists(Id) NOT NULL,
+	JourneyId INT FOREIGN KEY REFERENCES Journeys(Id) NOT NULL
+)
+
+-- Exercise 2
+INSERT INTO Planets ([Name]) VALUES
+	('Mars'),
+	('Earth'),
+	('Jupiter'),
+	('Saturn')
+
+INSERT INTO Spaceships ([Name], Manufacturer, LightSpeedRate) VALUES
+	('Golf',	'VW',	3),
+	('WakaWaka',	'Wakanda',	4),
+	('Falcon9',	'SpaceX',	1),
+	('Bed'	,'Vidolov',	6)
+
+-- Exercise 3
+UPDATE Spaceships
+SET LightSpeedRate += 1
+WHERE Id BETWEEN 8 AND 12
+
+-- Exercise 4
+DELETE FROM TravelCards
+WHERE JourneyId IN (1, 2, 3)
+
+DELETE FROM Journeys
+WHERE Id IN (1, 2, 3)
+
+-- Exercise 5
+  SELECT CardNumber, JobDuringJourney 
+    FROM TravelCards
+ORDER BY CardNumber
+
+-- Exercise 6
+  SELECT Id, CONCAT(FirstName, ' ', LastName) AS [FullName], Ucn
+    FROM Colonists
+ORDER BY FirstName, LastName, Id
+
+-- Exercise 7
+  SELECT Id, FORMAT(JourneyStart, 'dd/MM/yyyy') AS [JourneyStart], FORMAT(JourneyEnd, 'dd/MM/yyyy') AS [JourneyEnd]
+    FROM Journeys
+   WHERE Purpose = 'Military'
+ORDER BY JourneyStart
+
+-- Exercise 8
+  SELECT c.Id, CONCAT(c.FirstName, ' ', c.LastName) AS [full_name]
+    FROM TravelCards tc
+    JOIN Colonists c ON tc.ColonistId = c.Id 
+   WHERE JobDuringJourney = 'Pilot'
+ORDER BY c.Id
+
+-- Exercise 9
+SELECT COUNT(*) AS [count]
+  FROM Journeys j
+  JOIN TravelCards tc ON tc.JourneyId = j.Id
+  JOIN Colonists c ON c.Id = tc.ColonistId
+ WHERE j.Purpose = 'Technical'
+
+-- Exercise 10
+SELECT dt.[Name] AS [SpaceshipName], sp.[Name] AS [SpaceportName]
+FROM
+	  (SELECT TOP(1) * 
+	   FROM Spaceships
+	   ORDER BY LightSpeedRate DESC) AS dt
+JOIN Journeys j ON j.SpaceshipId = dt.Id
+JOIN Spaceports sp ON sp.Id = j.DestinationSpaceportId
+
+-- Exercise 11
+  SELECT s.[Name], s.Manufacturer
+    FROM Spaceships s
+    JOIN Journeys j ON s.Id = j.SpaceshipId
+    JOIN TravelCards tc ON tc.JourneyId = j.Id
+    JOIN Colonists c ON c.Id = tc.ColonistId
+   WHERE DATEDIFF(YEAR, c.BirthDate, '01/01/2019') < 30 AND tc.JobDuringJourney = 'Pilot'
+ORDER BY s.[Name]
+
+-- Exercise 12
+  SELECT p.[Name], sp.[Name]
+    FROM Planets p
+    JOIN Spaceports sp ON p.Id = sp.PlanetId
+    JOIN Journeys j ON j.DestinationSpaceportId = sp.Id
+   WHERE j.Purpose = 'Educational'
+ORDER BY sp.[Name] DESC
