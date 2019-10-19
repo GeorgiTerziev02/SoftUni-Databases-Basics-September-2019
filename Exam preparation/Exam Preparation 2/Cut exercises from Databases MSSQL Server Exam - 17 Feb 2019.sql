583a4d6ba6b247cadd4ba6d78da4e3dd67749cc9
Georgi Terziev <terzata2002@abv.bg>
2019-10-19 10:19:47 +0300

    only exercise 17 left

--- Exam preparation/Exam Preparation 2/Cut exercises from Databases MSSQL Server Exam - 17 Feb 2019.sql
+++ Exam preparation/Exam Preparation 2/Cut exercises from Databases MSSQL Server Exam - 17 Feb 2019.sql
@@ -22,19 +22,68 @@ ORDER BY FirstName
 GROUP BY t.FirstName, t.LastName, s.[Name], s.Lessons
 ORDER BY Students DESC, FullName, Subjects
 
-
 -- Exercise 11
-
-
+  SELECT TOP(10) t.FirstName, t.LastName,  
+		 COUNT(st.StudentId) AS [Students]
+    FROM Teachers t
+    JOIN StudentsTeachers st ON st.TeacherId = t.Id
+GROUP BY t.FirstName, t.LastName
+ORDER BY Students DESC, FirstName, LastName
 
 -- Exercise 13
-
-
+SELECT DISTINCT dt.FirstName, dt.LastName, dt.Grade 
+FROM
+	(SELECT s.FirstName, s.LastName, ss.Grade, ROW_NUMBER() OVER (PARTITION BY s.FirstName, s.LastName ORDER BY ss.Grade DESC) AS [Rank]
+	FROM Students s
+	JOIN StudentsSubjects ss ON s.Id = ss.StudentId) AS dt
+WHERE dt.[Rank] = 2
+ORDER BY dt.FirstName, dt.LastName
 
 -- Exercise 15
-
+SELECT dt.[Teacher Full Name], dt.SubjectName, dt.[Student Full Name], dt.Average
+FROM(
+SELECT s.Id,
+       CONCAT(s.FirstName, ' ', s.LastName) AS [Student Full Name],
+	   CONCAT(t.FirstName, ' ', t.LastName) AS [Teacher Full Name],
+	   su.[Name] AS [SubjectName],
+	   DENSE_RANK() OVER (PARTITION BY t.Id ORDER BY AVG(Grade) DESC) AS [Rank],
+	   CAST(AVG(Grade) AS DECIMAL(3,2)) AS [Average]
+  FROM Teachers t
+    JOIN StudentsTeachers AS st ON st.TeacherId = t.Id
+    JOIN Students AS s ON s.Id = st.StudentId
+    JOIN StudentsSubjects AS ss ON ss.StudentId = s.Id
+    JOIN Subjects AS su ON su.Id = ss.SubjectId AND su.Id = t.SubjectId
+GROUP BY s.Id, s.FirstName, s.LastName, t.Id, t.FirstName, t.LastName, su.[Name]) AS dt
+WHERE dt.[Rank] = 1
+ORDER BY dt.[SubjectName], dt.[Teacher Full Name], dt.Average DESC
 
 -- Exercise 17
 
 
 -- Exercise 20
+CREATE TABLE ExcludedStudents(
+	StudentId INT,
+	StudentName VARCHAR(50)
+)
+
+CREATE TRIGGER t_Exclude ON Students
+AFTER DELETE
+AS
+BEGIN
+	INSERT INTO ExcludedStudents (StudentId, StudentName)
+	       SELECT Id, CONCAT(FirstName,  ' ', LastName) FROM deleted	
+END
+
+DELETE FROM StudentsExams
+WHERE StudentId = 1
+
+DELETE FROM StudentsTeachers
+WHERE StudentId = 1
+
+DELETE FROM StudentsSubjects
+WHERE StudentId = 1
+
+DELETE FROM Students
+WHERE Id = 1
+
+SELECT * FROM ExcludedStudents
